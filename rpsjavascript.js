c7d5dad1ea057efb62356343c775599b22632203
konskabe <102515440+konskabe@users.noreply.github.com>
2024-10-23 13:26:05 +0300

    created interactive UI for RPS game

--- rpsjavascript.js
+++ rpsjavascript.js
@@ -7,59 +7,84 @@ function getComputerChoice() {
     }else{
         pick = "Scissors"
     }
-    console.log("I picked",pick);
+    document.getElementById("computerPick").innerHTML="I picked " + pick;
     return pick;
 }
+
 // console.log(getComputerChoice());
 
-function getHumanChoice(){
-    let choice = prompt("your move");
-    choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-    return choice;
-}
+// function getHumanChoice(){
+//     let choice = prompt("your move");
+//     choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+//     return choice;
+// }
 // console.log(getHumanChoice());
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice,computerChoice){
+function playRound(humanChoice){
+    let computerChoice = getComputerChoice();
+    let result = document.getElementById("result");
     if (computerChoice === "Rock"){
         if(humanChoice === "Paper"){
-            console.log("You Win! Paper beats Rock.");
+            result.innerHTML = "You win!";
             humanScore++;
         }else if(humanChoice === "Scissors"){
-            console.log("You lose! Rock beats Scissors");
+            result.innerHTML = "You lose!";
             computerScore++;
-        }else console.log("It's a draw.")
+        }else result.innerHTML = "Draw";
     }else if(computerChoice === "Paper"){
         if(humanChoice === "Rock"){
-            console.log("You Lose! Paper beats Rock.");
+            result.innerHTML = "You lose!";
             computerScore++;
         }else if (humanChoice === "Scissors"){
-            console.log("You win! Scissors beat Paper.");
+            result.innerHTML = "You win!";
             humanScore++;
-        }else console.log("It's a draw.")
+        }else result.innerHTML = "Draw";
     }else{//computerChoice === Scissors//
         if (humanChoice === "Rock"){
-            console.log("You win! Rock beats Scissors.");
+            result.innerHTML = "You win!";
             humanScore++;
         }else if (humanChoice === "Paper"){
-            console.log("You lose! Scissors beat Paper.");
+            result.innerHTML = "You lose!";
             computerScore++;
-        }else console.log("It's a draw.")
+        }else result.innerHTML = "Draw";
+    };
+    document.getElementById("computerScore").innerHTML = "Computer - " +computerScore;
+    document.getElementById("humanScore").innerHTML = "Human - " +humanScore;
+    if (computerScore === 5){
+        if(alert("You lost! " +computerScore + " to " +humanScore)){}
+        else window.location.reload();
+    }
+    if (humanScore === 5){
+        if(alert("You won! " +humanScore + " to " +computerScore)){}
+        else window.location.reload();
     }
 }
-function playgame(){
-    for (let i=0; i<5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection,computerSelection);
-    }
-    console.log("the final score is: Human-",humanScore," Computer-",computerScore);
-    if (humanScore>computerScore){
-        console.log("The winner is the Human");
-    }else if (humanScore<computerScore){
-        console.log("The winner is the Computer");
-    }else console.log("It's a Draw")
-    
-}
-playgame();
+
+// function playgame(){
+//     console.log("the final score is: Human-",humanScore," Computer-",computerScore);
+//     if (humanScore>computerScore){
+//         console.log("The winner is the Human");
+//     }else if (humanScore<computerScore){
+//         console.log("The winner is the Computer");
+//     }else console.log("It's a Draw")
+// }
+
+const rockButton = document.querySelector(".rockButton");
+const paperButton = document.querySelector(".paperButton");
+const scissorsButton = document.querySelector(".scissorsButton");
+
+rockButton.addEventListener("click", ()=> {
+    let humanChoice = "Rock";
+    playRound(humanChoice);
+});
+paperButton.addEventListener("click", function (){
+    let humanChoice = "Paper";
+    playRound(humanChoice);
+});
+scissorsButton.addEventListener("click", function (){
+    let humanChoice = "Scissors";
+    playRound(humanChoice);
+});
+
